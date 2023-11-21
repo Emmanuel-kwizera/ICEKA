9dec35dce690d90be0ee58e8b2b649e9d338129c
Kwizera Emmanuel <51673007+Emmanuel-kwizera@users.noreply.github.com>
2023-11-21 18:35:08 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-import Kidmend from "../assets/Intro.jpg";
+import Kidmend from "../assets/intro.jpg";
 
 const Home = () => {
   return (
